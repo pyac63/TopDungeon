58501dad6c5a516d850c0fa017c9e12c84728b87
Antoine <ant.games@outlook.fr>
2022-03-09 11:35:43 +0100

    adding player movement + video link

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,15 +4,42 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    //https://www.youtube.com/watch?v=b8YUfee_pzc
+    //00:37:18
+
+    private BoxCollider2D m_boxCollider;
+    private Vector3 m_moveDelta;
+    private float m_moveHorizontal;
+    private float m_moveVertical;
+
+    private void Start()
     {
-        
+        m_boxCollider = GetComponent<BoxCollider2D>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        m_moveHorizontal = Input.GetAxisRaw("Horizontal");
+        m_moveVertical = Input.GetAxisRaw("Vertical");
+    }
+
+
+    private void FixedUpdate()
+    {
+        //Reset move delta
+        m_moveDelta = new Vector3 (m_moveHorizontal, m_moveVertical, 0);
+
+        //Swap sprite direction left-right
+        if (m_moveDelta.x > 0)
+        {
+            transform.localScale = Vector3.one;
+        }
+        else if (m_moveDelta.x < 0)
+        {
+            transform.localScale = new Vector3(-1, 1, 1);
+        }
+
+        //Make this thing move
+        transform.Translate(m_moveDelta * Time.fixedDeltaTime);
     }
 }
