dbb5da145a45aad02a21b9bced9702a87bbb104b
Antoine <ant.games@outlook.fr>
2022-03-10 09:33:40 +0100

    movement script update + raycast check + sorting layers

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -9,6 +9,7 @@ public class Player : MonoBehaviour
 
     private BoxCollider2D m_boxCollider;
     private Vector3 m_moveDelta;
+    private RaycastHit2D m_hit;
 
     private float m_moveHorizontal;
     private float m_moveVertical;
@@ -28,7 +29,7 @@ public class Player : MonoBehaviour
     private void FixedUpdate()
     {
         //Reset move delta
-        m_moveDelta = new Vector3 (m_moveHorizontal, m_moveVertical, 0);
+        m_moveDelta = new Vector3(m_moveHorizontal, m_moveVertical, 0);
 
         //Swap sprite direction left-right
         if (m_moveDelta.x > 0)
@@ -40,7 +41,20 @@ public class Player : MonoBehaviour
             transform.localScale = new Vector3(-1, 1, 1);
         }
 
-        //Make this thing move
-        transform.Translate(m_moveDelta * Time.fixedDeltaTime);
+        //Make sure we can move in this direction
+        m_hit = Physics2D.BoxCast(transform.position, m_boxCollider.size, 0, new Vector2(0, m_moveDelta.y), Mathf.Abs(m_moveDelta.y * Time.deltaTime), LayerMask.GetMask("Characters", "Blocking"));
+        if (m_hit.collider == null)
+        {
+            //Make this thing move
+            transform.Translate(0, m_moveDelta.y * Time.fixedDeltaTime, 0);
+        }
+
+        m_hit = Physics2D.BoxCast(transform.position, m_boxCollider.size, 0, new Vector2(m_moveDelta.x, 0), Mathf.Abs(m_moveDelta.x * Time.deltaTime), LayerMask.GetMask("Characters", "Blocking"));
+        if (m_hit.collider == null)
+        {
+            //Make this thing move
+            transform.Translate(m_moveDelta.x * Time.fixedDeltaTime, 0, 0);
+        }
+
     }
 }
